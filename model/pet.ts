8fb981bbb4302cece2fd120c2d39136ff7ac9919
Asshley <asshleydeanwaynegozar@gmail.com>
2025-09-30 20:31:03 +0800

    feat(admin): implement home and pet listings page for admin - implement ui page for application tables and pet listing pages - implement model for querying all foster pets - implement server action for serving foster pets data - add tanstack query for fetching pets data - add skeleton shadcn component for handling loading states - add table for storing users application

--- model/pet.ts
+++ model/pet.ts
@@ -1,34 +1,58 @@
-import { pets, PrismaClient } from "@/app/generated/prisma";
-import { ModelResponse } from "./response";
+import { pets, PrismaClient } from '@/app/generated/prisma';
+import { ModelResponse } from './response';
 const prisma = new PrismaClient();
-
 
 // Get all not adopted pets
 export async function getAllAvailablePets(): Promise<ModelResponse<pets[]>> {
     try {
         const pets = await prisma.pets.findMany({
             where: {
-                pet_status: 'not_adopted'
-            }
-        })
+                pet_status: 'not_adopted',
+            },
+        });
 
         if (!pets || pets === null) {
             return {
                 success: false,
-                message: 'No pets found'
-            }
+                message: 'No pets found',
+            };
         }
         return {
             success: true,
             message: 'Query successful!',
-            data: pets
-        }
+            data: pets,
+        };
     } catch (error) {
         console.error(error);
         return {
             success: false,
             message: 'Query failed! An error occured',
-            error: `${error}`
-        }
+            error: `${error}`,
+        };
+    }
+}
+
+export async function getAllFosterPets(
+    userId: number
+): Promise<ModelResponse<pets[]>> {
+    try {
+        const fosterPets = await prisma.pets.findMany({
+            where: {
+                user_id: userId,
+            },
+        });
+
+        return {
+            success: true,
+            message: 'Successful query',
+            data: fosterPets,
+        };
+    } catch (error) {
+        console.error('An error occured while fetching data: ', error);
+        return {
+            success: false,
+            message: 'Failed to query data',
+            error: 'Database error',
+        };
     }
 }
