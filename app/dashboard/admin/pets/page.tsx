1725b17140eade6605a78a9d4eaa1369c63ee7e5
Asshley <asshleydeanwaynegozar@gmail.com>
2025-10-11 21:18:59 +0800

    feat(admin): update admin dashboard pages - add adoption management page for managing adoption applications - merge add pets and pet listings page for consistency and clarity - implement delete model and server for deleting pet - update add pet user interface - update side bar admin dashboard routes

--- app/dashboard/admin/pets/page.tsx
+++ app/dashboard/admin/pets/page.tsx
@@ -1,16 +1,18 @@
 'use client';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
+import AddPet from '@/components/AddPet';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Skeleton } from '@/components/ui/skeleton';
-import { getFosterPets } from '@/server/pets';
+import { deletePet, getFosterPets } from '@/server/pets';
 import { useQuery } from '@tanstack/react-query';
+import { ListFilter } from 'lucide-react';
+import { useTransition } from 'react';
+import { toast } from 'sonner';
 
 export default function PetPage() {
+    const [pending, startTransition] = useTransition();
     const queryPets = async () => {
         try {
             const response = await getFosterPets();
@@ -30,45 +32,73 @@ export default function PetPage() {
         queryFn: queryPets,
     });
     return (
-        <div className="grid grid-rows-[80px_1fr]">
-            <h1 className="font-poppins font-semibold text-4xl">
-                Pet Listings
-            </h1>
+        <div className="grid grid-rows-[80px_1fr] gap-4">
+            <nav className="flex justify-between items-center">
+                <h1 className="font-poppins font-semibold text-4xl">Pet Listings</h1>
+                <div className="grid grid-cols-3 justify-between gap-3">
+                    <div className="flex gap-2 items-center">
+                        <ListFilter />
+                        <Select name="sort">
+                            <SelectTrigger>
+                                <SelectValue placeholder="Filter by" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="male">Male</SelectItem>
+                                <SelectItem value="female">Female</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    </div>
+
+                    <Input type="text" placeholder="Search" />
+                    <AddPet />
+                </div>
+            </nav>
             <section className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
                 {data !== null && data !== undefined ? (
                     data.map((pet) => (
                         <Card key={pet.pet_id}>
                             <CardHeader className="text-center">
                                 <CardTitle>{pet.pet_name}</CardTitle>
-                                <CardDescription>
-                                    {pet.pet_species}
-                                </CardDescription>
+                                <CardDescription>{pet.pet_species}</CardDescription>
                             </CardHeader>
                             <hr />
-                            <CardContent>
+                            <CardContent className="grid gap-2">
                                 <ul>
                                     <li className="grid grid-cols-2">
-                                        <div>Age: </div>
-                                        <div>{pet.pet_age}</div>
+                                        <div className="text-[0.8rem] font-semibold">Age: </div>
+                                        <div className="text-[0.8rem]">{pet.pet_age}</div>
                                     </li>
                                     <li className="grid grid-cols-2">
-                                        <div>Pet status: </div>
-                                        <div>
-                                            {pet.pet_status === 'not_adopted'
-                                                ? 'Not Adopted'
-                                                : 'Adopted'}
+                                        <div className="text-[0.8rem] font-semibold">Pet status: </div>
+                                        <div className="text-[0.8rem]">
+                                            {pet.pet_status === 'not_adopted' ? 'Not Adopted' : 'Adopted'}
                                         </div>
                                     </li>
                                     <li className="grid grid-cols-2">
-                                        <div>Breed: </div>
-                                        <div>{pet.pet_breed}</div>
-                                    </li>
-                                    <li className="grid grid-cols-2">
-                                        <div>Description: </div>
-                                        <div>{pet.pet_description}</div>
+                                        <div className="text-[0.8rem] font-semibold">Breed: </div>
+                                        <div className="text-[0.8rem]">{pet.pet_breed}</div>
                                     </li>
                                 </ul>
                             </CardContent>
+                            <CardFooter className="grid grid-rows-2 gap-3">
+                                <Button variant={'default'} className="font-semibold">
+                                    Update
+                                </Button>
+                                <Button
+                                    variant={'outline'}
+                                    onClick={() => {
+                                        startTransition(async () => {
+                                            const removePet = await deletePet(pet.pet_id);
+
+                                            if (removePet.success) {
+                                                toast('Pet deleted successfully!');
+                                            }
+                                        });
+                                    }}
+                                >
+                                    Delete
+                                </Button>
+                            </CardFooter>
                         </Card>
                     ))
                 ) : (
