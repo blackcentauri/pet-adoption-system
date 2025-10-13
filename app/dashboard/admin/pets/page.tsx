3b7fd56eb50f3ba678b40d6bb9e39f8e91a1af9d
Asshley <asshleydeanwaynegozar@gmail.com>
2025-10-13 22:17:16 +0800

    feat: add invalidate cache and migrate to tanstack mutation for performance efficiency

--- app/dashboard/admin/pets/page.tsx
+++ app/dashboard/admin/pets/page.tsx
@@ -1,33 +1,61 @@
 'use client';
+import { pets } from '@/app/generated/prisma';
 import AddPet from '@/components/AddPet';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { Skeleton } from '@/components/ui/skeleton';
 import { deletePet, getFosterPets } from '@/server/pets';
 import { useQuery } from '@tanstack/react-query';
 import { ListFilter } from 'lucide-react';
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 import { toast } from 'sonner';
 
 export default function PetPage() {
     const [pending, startTransition] = useTransition();
+    const [allPets, setAllPets] = useState<pets[]>([]);
+    const [pets, setPets] = useState<pets[]>([]);
+    const [search, setSearch] = useState('');
     const queryPets = async () => {
         try {
             const response = await getFosterPets();
 
             if (!response.success) {
-                return null;
+                return [];
             }
-
+            setAllPets(response.data ? response.data : []);
+            setPets(response.data ? response.data : []);
             return response.data;
         } catch (error) {
             console.error('An error occured: ', error);
         }
     };
 
-    const { data } = useQuery({
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setSearch(value);
+
+        if (!value.trim()) {
+            setPets(allPets);
+            return;
+        }
+
+        const filteredPets = allPets.filter((pet) =>
+            [pet.pet_name, pet.pet_species, pet.pet_breed, pet.pet_status].some((field) =>
+                field?.toLowerCase().includes(value.toLowerCase())
+            )
+        );
+
+        setPets(filteredPets);
+    };
+
+    const handleSort = (value: string) => {
+        const sortedPets = [...allPets].sort((a, b) =>
+            a.pet_name.localeCompare(b.pet_name, undefined, { sensitivity: 'base' })
+        );
+        setPets(value === 'asc' ? sortedPets : sortedPets.reverse());
+    };
+    useQuery({
         queryKey: ['pets'],
         queryFn: queryPets,
     });
@@ -38,72 +66,71 @@ export default function PetPage() {
                 <div className="grid grid-cols-3 justify-between gap-3">
                     <div className="flex gap-2 items-center">
                         <ListFilter />
-                        <Select name="sort">
+                        <Select name="sort" onValueChange={handleSort}>
                             <SelectTrigger>
                                 <SelectValue placeholder="Filter by" />
                             </SelectTrigger>
                             <SelectContent>
-                                <SelectItem value="male">Male</SelectItem>
-                                <SelectItem value="female">Female</SelectItem>
+                                <SelectItem value="asc">ASC</SelectItem>
+                                <SelectItem value="desc">DESC</SelectItem>
                             </SelectContent>
                         </Select>
                     </div>
 
-                    <Input type="text" placeholder="Search" />
+                    <Input type="text" placeholder="Search" value={search} onChange={handleSearch} />
+
                     <AddPet />
                 </div>
             </nav>
             <section className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-4">
-                {data !== null && data !== undefined ? (
-                    data.map((pet) => (
-                        <Card key={pet.pet_id}>
-                            <CardHeader className="text-center">
-                                <CardTitle>{pet.pet_name}</CardTitle>
-                                <CardDescription>{pet.pet_species}</CardDescription>
-                            </CardHeader>
-                            <hr />
-                            <CardContent className="grid gap-2">
-                                <ul>
-                                    <li className="grid grid-cols-2">
-                                        <div className="text-[0.8rem] font-semibold">Age: </div>
-                                        <div className="text-[0.8rem]">{pet.pet_age}</div>
-                                    </li>
-                                    <li className="grid grid-cols-2">
-                                        <div className="text-[0.8rem] font-semibold">Pet status: </div>
-                                        <div className="text-[0.8rem]">
-                                            {pet.pet_status === 'not_adopted' ? 'Not Adopted' : 'Adopted'}
-                                        </div>
-                                    </li>
-                                    <li className="grid grid-cols-2">
-                                        <div className="text-[0.8rem] font-semibold">Breed: </div>
-                                        <div className="text-[0.8rem]">{pet.pet_breed}</div>
-                                    </li>
-                                </ul>
-                            </CardContent>
-                            <CardFooter className="grid grid-rows-2 gap-3">
-                                <Button variant={'default'} className="font-semibold">
-                                    Update
-                                </Button>
-                                <Button
-                                    variant={'outline'}
-                                    onClick={() => {
-                                        startTransition(async () => {
-                                            const removePet = await deletePet(pet.pet_id);
+                {pets.map((pet) => (
+                    <Card key={pet.pet_id}>
+                        <CardHeader className="text-center">
+                            <CardTitle>{pet.pet_name}</CardTitle>
+                            <CardDescription>{pet.pet_species}</CardDescription>
+                        </CardHeader>
+                        <hr />
+                        <CardContent className="grid gap-2">
+                            <ul>
+                                <li className="grid grid-cols-2">
+                                    <div className="text-[0.8rem] font-semibold">Age: </div>
+                                    <div className="text-[0.8rem]">{pet.pet_age}</div>
+                                </li>
+                                <li className="grid grid-cols-2">
+                                    <div className="text-[0.8rem] font-semibold">Pet status: </div>
+                                    <div className="text-[0.8rem]">
+                                        {pet.pet_status === 'not_adopted' ? 'Not Adopted' : 'Adopted'}
+                                    </div>
+                                </li>
+                                <li className="grid grid-cols-2">
+                                    <div className="text-[0.8rem] font-semibold">Breed: </div>
+                                    <div className="text-[0.8rem]">{pet.pet_breed}</div>
+                                </li>
+                            </ul>
+                        </CardContent>
+                        <CardFooter className="grid grid-rows-2 gap-3">
+                            <Button variant={'default'} className="font-semibold">
+                                Update
+                            </Button>
+                            <Button
+                                variant={'outline'}
+                                onClick={() => {
+                                    startTransition(async () => {
+                                        const removePet = await deletePet(pet.pet_id);
 
-                                            if (removePet.success) {
-                                                toast('Pet deleted successfully!');
-                                            }
-                                        });
-                                    }}
-                                >
-                                    Delete
-                                </Button>
-                            </CardFooter>
-                        </Card>
-                    ))
-                ) : (
-                    <Skeleton className="h-full w-full" />
-                )}
+                                        if (removePet.success) {
+                                            toast('Pet deleted successfully!');
+                                        }
+
+                                        setPets(pets.filter((removePet) => removePet.pet_id !== pet.pet_id));
+                                    });
+                                }}
+                            >
+                                Delete
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                ))}
             </section>
         </div>
     );
--- components/AddPet.tsx
+++ components/AddPet.tsx
@@ -1,51 +1,35 @@
 'use client';
-import { Form, FormError, FormGroup, FormLabel } from '@/components/Form';
+import { Form, FormGroup, FormLabel } from '@/components/Form';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
 import { insertPet } from '@/server/pets';
-import { ActionResponse } from '@/server/response';
 import { Dialog } from '@radix-ui/react-dialog';
-import { Loader2Icon } from 'lucide-react';
-import { useActionState } from 'react';
 import { toast } from 'sonner';
 import { DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
-
-const initialState: ActionResponse = {
-    success: false,
-    message: '',
-    error: '',
-};
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function AddPet() {
-    const handleSubmit = async (prevState: ActionResponse, formData: FormData) => {
+    const queryClient = useQueryClient();
+    const mutation = useMutation({
+        mutationFn: async (formData: FormData) => {
+            return insertPet(formData);
+        },
+        onSuccess: () => {
+            toast('Created successfully');
+            queryClient.invalidateQueries({ queryKey: ['pets'] });
+        },
+    });
+
+    const handleSubmit = async (data: FormData) => {
         try {
-            const pet = await insertPet(formData);
-
-            if (!pet.success) {
-                return {
-                    success: false,
-                    message: 'Failed to insert pet',
-                };
-            }
-
-            toast('Created Successfully!');
-
-            return {
-                success: true,
-                message: 'Created Successfully!',
-            };
+            await mutation.mutateAsync(data);
         } catch (error) {
-            console.error('An error occured:', error);
-            return {
-                success: false,
-                message: 'Failed to insert',
-                error: 'Insertion error',
-            };
+            console.error('An error occured: ', error);
         }
     };
-    const [state, formAction, isPending] = useActionState<ActionResponse, FormData>(handleSubmit, initialState);
+
     return (
         <Dialog>
             <DialogTrigger className="bg-[#FED200] hover:bg-[#e3ba00] text-white font-semibold rounded-xl px-14 shadow-md transition-all">
@@ -56,40 +40,26 @@ export default function AddPet() {
                     <DialogTitle className="font-poppins font-medium text-2xl">New Pet</DialogTitle>
                     <DialogDescription>Post new foster pet</DialogDescription>
                 </DialogHeader>
-                <FormError className="text-red-500">{state.error ? state.error : null}</FormError>
-                <Form className="grid gap-2" action={formAction}>
+
+                <Form className="grid gap-2" action={handleSubmit}>
                     <FormGroup className="grid grid-cols-2 gap-3">
                         <FormGroup className="grid gap-2">
                             <FormLabel>Pet name</FormLabel>
-                            <Input
-                                id="name"
-                                name="name"
-                                type="text"
-                                placeholder="Pet name"
-                                required
-                                disabled={isPending}
-                            />
+                            <Input id="name" name="name" type="text" placeholder="Pet name" required />
                         </FormGroup>
                         <FormGroup className="grid gap-2">
                             <FormLabel>Species</FormLabel>
-                            <Input
-                                id="species"
-                                name="species"
-                                type="text"
-                                placeholder="Species"
-                                required
-                                disabled={isPending}
-                            />
+                            <Input id="species" name="species" type="text" placeholder="Species" required />
                         </FormGroup>
                     </FormGroup>
                     <FormGroup className="grid grid-cols-2 gap-3">
                         <FormGroup className="grid gap-2">
                             <FormLabel>Age</FormLabel>
-                            <Input id="age" name="age" type="text" placeholder="Age" required disabled={isPending} />
+                            <Input id="age" name="age" type="text" placeholder="Age" required />
                         </FormGroup>
                         <FormGroup className="grid gap-2 w-full">
                             <FormLabel>Sex</FormLabel>
-                            <Select name="sex" required disabled={isPending}>
+                            <Select name="sex" required>
                                 <SelectTrigger>
                                     <SelectValue placeholder="Sex" />
                                 </SelectTrigger>
@@ -103,28 +73,15 @@ export default function AddPet() {
                     <FormGroup className="grid grid-cols-2 gap-2">
                         <FormGroup className="grid gap-2">
                             <FormLabel>Breed</FormLabel>
-                            <Input
-                                id="breed"
-                                name="breed"
-                                type="text"
-                                placeholder="Pet breed"
-                                required
-                                disabled={isPending}
-                            />
+                            <Input id="breed" name="breed" type="text" placeholder="Pet breed" required />
                         </FormGroup>
                     </FormGroup>
                     <FormGroup className="grid gap-2">
                         <FormLabel>Description</FormLabel>
-                        <Textarea
-                            id="description"
-                            name="description"
-                            placeholder="Description"
-                            required
-                            disabled={isPending}
-                        />
+                        <Textarea id="description" name="description" placeholder="Description" required />
                     </FormGroup>
                     <Button variant={'default'} className="mt-4">
-                        {isPending ? <Loader2Icon className="animate-spin" /> : <span>Save</span>}
+                        Save
                     </Button>
                 </Form>
             </DialogContent>
