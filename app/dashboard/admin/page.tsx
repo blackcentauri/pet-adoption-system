8d0639df1ffde06fc842591d8d3be6516817ab00
Asshley <asshleydeanwaynegozar@gmail.com>
2025-10-14 14:50:42 +0800

    feat(admin): implement home dashboard analytics - add badge and chart ui from shadcn for displaying graphs and data - implement model for querying applications count - implement server action for fetching data from model - implement ui home page dashboard for admin analytics

--- app/dashboard/admin/page.tsx
+++ app/dashboard/admin/page.tsx
@@ -1,3 +1,13 @@
+'use client';
+
+import AdminSectionCards from '@/components/Admin-Section-Cards';
+import { ChartAreaInteractive } from '@/components/Analytics-Chart';
+
 export default function AdminHomePage() {
-    return <div>Home Analytics</div>;
+    return (
+        <div className='grid gap-8'>
+            <AdminSectionCards />
+            <ChartAreaInteractive />
+        </div>
+    );
 }
